79c65d846d6dcc94018bc2137010d1a7a426d8e8
VadimsVladisevs <vadims.vladisevs@gmail.com>
2022-06-12 23:02:20 +0300

    Add MongoDB connection

--- app.js
+++ app.js
@@ -1,8 +1,17 @@
+const {
+  findAllBooks,
+  findBooksByStatus
+} = require('./modules/db.js');
+
+const fs = require('node:fs/promises');
+
 const express = require('express');
 const https = require('https');
 const bodyParser = require("body-parser");
 const mongoose = require('mongoose');
+
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.static("public"));
 
@@ -10,11 +19,27 @@ app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-// mongoose.connect('mongodb+srv://admin:sta08rtdf@notyoung-reader.be5jr.mongodb.net/readersDB');
+app.get('/books', function(req, res) {
+  const status = req.query.status;
+
+  if (status) {
+    findBooksByStatus(status).then(function(foundBooks) {
+      res.send(foundBooks);
+    }).catch(function(err) {
+      res.send(err);
+    });
+  } else {
+    findAllBooks().then(function(foundBooks) {
+      res.send(foundBooks);
+    }).catch(function(err) {
+      res.send(err);
+    });
+  }
+});
 
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/index.html");
-})
+});
 
 app.listen(process.env.PORT || 3000, function() {
   console.log("Server is running on port 3000!");
